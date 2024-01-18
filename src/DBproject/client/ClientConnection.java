045fb869fba6a674ed318ee19c6c546f91a0b352
Marro64 <marrobot@gmail.com>
2024-01-18 10:38:01 +0100

    Implemented Client and ClientConnection

--- src/DBproject/client/ClientConnection.java
+++ src/DBproject/client/ClientConnection.java
@@ -1,24 +1,28 @@
 package DBproject.client;
 
+import DBproject.networking.Protocol;
 import DBproject.networking.SocketConnection;
+
 import java.io.IOException;
 
 /**
  * uses the Protocol to communicate with a server using the framework of SocketConnection.
  */
 public class ClientConnection extends SocketConnection {
-    private Client client;
+    private final Client client;
+
     public ClientConnection(String host, int port, Client client) throws IOException {
         super(host, port);
         this.client = client;
+        super.start();
     }
 
     /**
      * Close the network connection. This will also cause the thread that receives messages to stop.
      */
     @Override
-    public void close(){
-
+    public void close() {
+        super.close();
     }
 
     /**
@@ -28,7 +32,46 @@ public class ClientConnection extends SocketConnection {
      */
     @Override
     protected void handleMessage(String message) {
-
+        String[] messageParts = message.split(Protocol.SEPARATOR);
+        try {
+            String header = messageParts[0];
+            switch (header) {
+                case Protocol.HELLO:
+                    if (messageParts.length < 2) {
+                        throw new IllegalArgumentException();
+                    }
+                    String serverDescription = messageParts[1];
+                    client.receiveHello(serverDescription);
+                    break;
+                case Protocol.LOGIN:
+                    client.receiveLoginConfirmation();
+                    break;
+                case Protocol.ALREADYLOGGEDIN:
+                    client.receiveAlreadyLoggedIn();
+                    break;
+                case Protocol.NEWGAME:
+                    if (messageParts.length < 3) {
+                        throw new IllegalArgumentException();
+                    }
+                    String player1 = messageParts[1];
+                    String player2 = messageParts[2];
+                    client.receiveNewGame(player1, player2);
+                    break;
+                case Protocol.MOVE:
+                    if (messageParts.length < 2) {
+                        throw new IllegalArgumentException();
+                    }
+                    int location = Integer.parseInt(messageParts[1]);
+                    client.receiveMove(location);
+                    break;
+                case Protocol.ERROR:
+                    client.receiveError();
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException ignore) {
+        }
     }
 
     /**
@@ -36,23 +79,32 @@ public class ClientConnection extends SocketConnection {
      */
     @Override
     protected void handleDisconnect() {
-
+        client.handleDisconnect();
     }
 
-    public void sendHello(String clientDescription){
-
+    /*@ requires !clientDescription.contains("~");
+    */
+    public void sendHello(String clientDescription) {
+        assert !clientDescription.contains("~");
+        super.sendMessage(Protocol.HELLO + Protocol.SEPARATOR + clientDescription);
     }
 
-    public void sendLogin(String username){
-
+    /*@ requires !username.contains("~");
+    */
+    public void sendLogin(String username) {
+        assert !username.contains("~");
+        super.sendMessage(Protocol.HELLO + Protocol.SEPARATOR + username);
     }
-    public void sendList(){
 
+    public void sendList() {
+        super.sendMessage(Protocol.LIST);
     }
-    public void sendQueue(){
 
+    public void sendQueue() {
+        super.sendMessage(Protocol.QUEUE);
     }
-    public void sendMove(int location){
 
+    public void sendMove(int location) {
+        super.sendMessage(Protocol.MOVE + Protocol.SEPARATOR + location);
     }
 }
