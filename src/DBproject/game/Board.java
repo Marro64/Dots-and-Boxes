8d6b45e6407514fd102aedef72bec9659a03f536
s2613468 <y.vandekerk@student.utwente.nl>
2024-01-29 10:06:20 +0100

    boxes with a colour

--- src/DBproject/game/Board.java
+++ src/DBproject/game/Board.java
@@ -17,6 +17,14 @@ public class Board {
             " 27     28     29     30     31     32  ", " .  33  .  34  .  35  .  36  .  37  . ",
             " 38     39     40     41     42     43  ", " .  44  .  45  .  46  .  47  .  48  . ",
             " 49     50     51     52     53     54  ", " .  55  .  56  .  57  .  58  .  59  . "};
+
+    // Declaring ANSI_RESET so that we can reset the color
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    // Declaring the colors
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RED ="\u001B[31m";
+
     /*@
         public invariant lines.length < (DIM-1)*DIM + (DIM-1)*DIM ;
         public invariant boxes.length < (DIM-1)*(DIM-1);
@@ -576,9 +584,13 @@ public class Board {
                     } else {
                         row += "  " + " ";
                     }
-                    if (!(getBox(i / 2, j) == 0)) {
-                        row += getBox(i / 2, j) + "  ";
-                    } else {
+                    if (getBox(i/2,j)==1){
+                        row += ANSI_RED + getBox(i/2, j) + ANSI_RESET + "  ";
+                    }
+                    else if (getBox(i/2,j)==2){
+                        row += ANSI_BLUE + getBox(i/2, j) + ANSI_RESET + "  ";
+                    }
+                    else {
                         row += " " + "  ";
                     }
                 }
